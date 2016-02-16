8041201736a5cb819b8be001db878c6cc471ce75
Frida <friekl@kth.se>
2016-02-16 14:58:19 +0100

    hihi

--- js/view/dinnerView4.js
+++ js/view/dinnerView4.js
@@ -15,29 +15,35 @@ var DinnerView4 = function (container, model) {
 
 	this.dishes.empty();
 
+	var sprice = model.getTotalDishPrice(this.starter.id)
+	if(!(sprice)){ sprice = 0}
 	this.dishes.append(
-			"<div class="+'"'+"dish"+" "+"col-md-4"+'"'+">"+
+			"<div class="+'"'+"confirmed-dish"+" "+"col-md-4"+'"'+">"+
 			"<h4>Starter</h4>"+
 			"<img src="+'"'+"images/"+this.starter.image+'"'+"id="+'"'+"image"+'"'+">"
 			+"<h5 id="+'"'+"title"+'"'+">"+this.starter.name+"</h5>"
-			+"<div class="+'"'+"price"+'"'+">"+"<p>"+model.getTotalDishPrice(this.starter.id)+" SEK"+"</p>"+"</div>"
+			+"<div class="+'"'+"price"+'"'+">"+"<p>"+sprice+" SEK"+"</p>"+"</div>"
 			+"</div>");
 
+	var mprice = model.getTotalDishPrice(this.main.id)
+	if(!(mprice)){ mprice = 0}
 	this.dishes.append(
-			"<div class="+'"'+"dish"+" "+"col-md-4"+'"'+">"+
+			"<div class="+'"'+"confirmed-dish"+" "+"col-md-4"+'"'+">"+
 			"<h4>Main</h4>"+
 			"<img src="+'"'+"images/"+this.main.image+'"'+"id="+'"'+"image"+'"'+">"
 			+"<h5 id="+'"'+"title"+'"'+">"+this.main.name+"</h5>"
-			+"<div class="+'"'+"price"+'"'+">"+"<p>"+model.getTotalDishPrice(this.main.id)+" SEK"+"</p>"+"</div>"
+			+"<div class="+'"'+"price"+'"'+">"+"<p>"+mprice+" SEK"+"</p>"+"</div>"
 			+"</div>");
 
 
+	var dprice = model.getTotalDishPrice(this.dessert.id)
+	if(!(dprice)){ dprice = 0}
 	this.dishes.append(
-			"<div class="+'"'+"dish"+" "+"col-md-4"+'"'+">"+
+			"<div class="+'"'+"confirmed-dish"+" "+"col-md-4"+'"'+">"+
 			"<h4>Dessert</h4>"+
 			"<img src="+'"'+"images/"+this.dessert.image+'"'+"id="+'"'+"image"+'"'+">"
 			+"<h5 id="+'"'+"title"+'"'+">"+this.dessert.name+"</h5>"
-			+"<div class="+'"'+"price"+'"'+">"+"<p>"+model.getTotalDishPrice(this.dessert.id)+" SEK"+"</p>"+"</div>"
+			+"<div class="+'"'+"price"+'"'+">"+"<p>"+dprice+" SEK"+"</p>"+"</div>"
 			+"</div>");
 
 	this.totalPrice.html("Totalt pris:"+"\n"+model.getTotalMenuPrice(model.getAllIngredients())+" SEK");
